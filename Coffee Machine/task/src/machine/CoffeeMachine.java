d7b5f88f98e7184ed2ae3eca95629ea347dc498f
Mikhail <bobrov.mikhail94@gmail.com>
2021-08-14 18:42:15 +0300

    package test 3

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,5 +3,7 @@ package machine;
 public class CoffeeMachine {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("dafdaffy");
     }
+
 }
